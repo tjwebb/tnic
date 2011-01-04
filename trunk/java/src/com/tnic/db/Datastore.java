61d2bf1d723956b20d7cc07c2000f4c4ca0523aa
abshnasko <abshnasko@44661275-7af0-b8cb-fcaa-37a6090f65ab>
2011-01-04 05:21:25 +0000

    working, but useless build

---
+++ trunk/java/src/com/tnic/db/Datastore.java
@@ -0,0 +1,8 @@
+package com.tnic.db;
+
+public interface Datastore {
+    /* javascript namespace queries
+    public Namespace getNamespace(String ns);
+    public Function getFunction(String fun);
+    */
+}

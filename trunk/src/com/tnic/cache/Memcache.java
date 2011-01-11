22ff28b9d4e80c37ba0160cacdc4ce5e4d2bbf8b
abshnasko <abshnasko@44661275-7af0-b8cb-fcaa-37a6090f65ab>
2011-01-11 05:43:58 +0000

    clear

--- trunk/src/com/tnic/cache/Memcache.java
+++
@@ -1,7 +0,0 @@
-package com.tnic.cache;
-
-public interface Memcache {
-    public boolean put (String key, Object value);
-    public Object get (String key);
-    public Object remove (String key);
-}
--- trunk/src/com/tnic/db/Datastore.java
+++
@@ -1,8 +0,0 @@
-package com.tnic.db;
-
-public interface Datastore {
-    /* javascript namespace queries
-    public Namespace getNamespace(String ns);
-    public Function getFunction(String fun);
-    */
-}

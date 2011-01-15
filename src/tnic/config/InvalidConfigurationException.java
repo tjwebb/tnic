13dc744ae4198c48e5b897749506fdf737233b94
abshnasko <abshnasko@44661275-7af0-b8cb-fcaa-37a6090f65ab>
2011-01-15 21:16:23 +0000

    working editor

---
+++ src/tnic/config/InvalidConfigurationException.java
@@ -0,0 +1,7 @@
+package tnic.config;
+
+public class InvalidConfigurationException extends Exception {
+    public InvalidConfigurationException (String message) {
+        super(message);
+    }
+}

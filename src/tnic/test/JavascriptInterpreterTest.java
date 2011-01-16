98c96b7766910c4a5f82ec92f34259256b9f8712
abshnasko <abshnasko@44661275-7af0-b8cb-fcaa-37a6090f65ab>
2011-01-16 20:38:10 +0000

    tmp

--- src/tnic/test/JavascriptInterpreterTest.java
+++ src/tnic/test/JavascriptInterpreterTest.java
@@ -1,5 +1,7 @@
 package tnic.test;
 
+import tnic.jsvm.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,30 +13,12 @@ import java.io.IOException;
 public class JavascriptInterpreterTest extends HttpServlet {
     public void doGet (HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
+        CompiledScript script = TnicJavascriptEngine.compile(
+            "(function () { return $argv.x; })();"
+        );
+        Object result = TnicJavascriptEngine.eval(script, "({ x: 5 })");
         resp.setContentType("text/plain");
             
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
-        Compilable compiler = (Compilable)engine;
-        Object result1 = null;
-        Object result2 = null;
-        Object result3 = null;
-        try {
-            CompiledScript script1 = compiler.compile("var x = (function() { return { value : 'ok1' }; })()");
-            CompiledScript script2 = compiler.compile("function y (obj) { return obj.value; }");
-            CompiledScript script3 = compiler.compile("(function () { return 4208; })()");
-            script1.eval();
-            script2.eval();
-            Invocable inv = (Invocable)engine;
-            result1 = engine.get("x");
-            result2 = inv.invokeFunction("y", result1);
-            result3 = script3.eval();
-        }
-        catch (Exception e) {
-            resp.getWriter().println(e.toString());
-        }
-        resp.getWriter().println(result1.toString());
-        resp.getWriter().println(result2.toString());
-        resp.getWriter().println(result3.toString());
+        resp.getWriter().println(result.toString());
     }
 }
